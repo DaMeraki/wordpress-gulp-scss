645cc78f5f8db08ad0b1b8d28ef3a3786934bd4b
DaMeraki <artsoulxx@gmail.com>
2022-10-06 16:09:24 +0100

    theme support, styles and scripts added

---
+++ wp-content/themes/riker-theme/functions.php
@@ -0,0 +1,40 @@
+<?php
+
+// Setting up initials for our riker theme
+function riker_basics() {
+
+	add_theme_support( 'title-tag' );
+    add_theme_support( 'post-thumbnails' );
+
+}
+add_action( 'after_setup_theme', 'riker_basics' );
+
+// Loading our riker theme styles
+function riker_styles() {
+
+    $version = wp_get_theme()->get('version'); 
+    wp_enqueue_style( 'main-css', get_stylesheet_directory_uri() . '/assets/css/style.css', [ 'reset-css' ], $version, 'all' );
+    wp_enqueue_style( 'reset-css', get_stylesheet_directory_uri() . '/assets/css/reset.css', array(), $version, 'all' );
+
+}
+add_action( 'wp_enqueue_scripts', 'riker_styles' );
+
+// Loading our riker theme scripts
+function riker_scripts() {
+
+}
+add_action( 'wp_enqueue_scripts', 'riker_scripts' );
+
+// Setting up our riker sidebar
+function riker_sidebar() {
+    register_sidebar([
+      'name'          => esc_html__( 'Main Sidebar', 'riker-theme' ),
+      'id'            => 'main-sidebar',
+      'description'   => esc_html__( 'Add widgets for main sidebar here', 'riker-theme' ),
+      'before_widget' => '<section class="widget">',
+      'after_widget'  => '</section>',
+      'before_title'  => '<h2 class="widget-title aside-heading">',
+      'after_title'   => '</h2>',
+    ]);
+}
+add_action( 'widgets_init', 'riker_sidebar' );
